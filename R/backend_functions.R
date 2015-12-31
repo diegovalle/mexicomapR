6a313107456262e38016716088375be2d743a066
Eduardo Flores <eduardo@enelmargen.org>
2015-12-31 13:04:35 -0600

    dev

--- R/backend_functions.R
+++ R/backend_functions.R
@@ -1,15 +1,40 @@
-#' Attempts to fix INEGI number to description of geography
+#' Checks and cleans format of data.frame to be merged with fortified data
 #'
-#' Calls \code{data(mxgeography)} and attempts to translate geography names to INEGI codes. 
+#' Checks unique id's, format of value and other possible issues.  
 #' 
-#' @param vector Vector of names or descriptions
+#' @param df Data.frame with id and value columns
+#' @param vcol Value column with standard evaluation. Defaults to "value".
+#' @param nobject length of type of map (32 for state or 2457 for municipality)
 #' @author Eduardo Flores
 #' @examples
 #' 
 #' @export
 #'
 #'
-mxfind_id <- function(vector){
-  v <- tolower(vector)
-  v <- unique(v)
+mx_checkdf <- function(df, vcol = "value", nobject = 32){
+ # Check if df is a data.frame and has the proper names 
+  if(base::class(df) == "data.frame"){
+    # clean and fix data frame
+    base::names(df) <- base::tolower(names(df))
+    
+    if(!("id" %in% base::names(df))){stop("id column not found in data")}
+    if(!(vcol %in% base::names(df))){stop("value column not found in data")}
+  }
+  
+  # Check NA in value columns. 
+  row.has.na <- apply(df, 1, function(x) {any(is.na(x))})
+  if (sum(row.has.na) > 0) {r <- TRUE}else{r <- FALSE}
+ 
+  # the object "r" has a false or true parameter...
+  # now, we validate.
+  if(r){stop("NAs in data.frame")}
+  
+  # Check if all the ids are unique 
+  if(length(unique(df$id)) == length(df$id)){}else{stop("ids should be unique")}
+  
+  # Check length matches type of map
+  if(nobject == length(df$id)){}else{
+    stop("number of ids do not match map type (must have 32 for states or 2,457 for municipalities)")}
+
+  # all checks passed...
 }
--- R/user_functions.R
+++ R/user_functions.R
@@ -1,129 +1,138 @@
-#' Creates a static map
+#' Creates a map
 #'
 #' Calls \code{mxmap_state} or \code{mxmap_municipality} to generate a static map. 
 #' 
 #' @param type Type of map to create (data by state or municipality)
 #' @param df Data frame or vector with information to paint geography. The data must contain an "id" and a "value" column to be matched.
-#' @param geography Name of geography (i.e. "Aguascalientes"). Names are searched in \code{data(mxgeography)}. Default is set to = "all", to create a country-wide map.
+#' @param method Method to be used (static for ggplot2 object, leaflet for leaflet object)
 #' @author Eduardo Flores
 #' @return ggplot object
 #' @seealso mxmap_state_p, mxmap_state_l, mxmap_state_g, mxmap_municipality_p, mxmap_municipality_l, mxmap_municipality_g
 #' @examples
-#' \dontrun{
-#' population_choroplethr <- mxmap("state", data(mxpop2010))
-#' }
+#' # A static state map with random data 
+#' data(states_catalog, package = "mexicomapR")
+#' df <- data.frame("id" = states_catalog$id, "value" = runif(n = 32, min = 1, max = 100))
+#' mxmap(type = "state", df)
 #' @export
 #'
-mxmap <- function(type, df, geography = "all", method = "ggplot"){
+mxmap <- function(type, df, method = "static", vcol = "value"){
   type <- tolower(type)
+  # Function designed to call others. 
+  # A standard way to create many types of maps.
 
   if(type == "state"){
-    if(method == "ggplot"){
-      m <- mexicomapR::mxmap_state_p(df)  
+    if(method == "static"){
+      message("Using mxmap_state_s")
+      m <- mexicomapR::mxmap_state_s(df, vcol = vcol)  
     }else{
       if(method == "leaflet"){
-        m <- mexicomapR::mxmap_state_l(df)
-      }else{
-        if(method == "google"){
-          m <- mexicomapR::mxmap_state_g(df)
-        }else{stop("Metod not recognized. Use gglot, leaflet or google")}
-        
-      }
+        message("Using mxmap_state_l")
+        m <- mexicomapR::mxmap_state_l(df, vcol = vcol)}else{
+          stop("Metod not recognized. Use static, leaflet or google")}
     }
   }else{
   if(type == "municipality"){
-    if(method == "ggplot"){
-      m <- mexicomapR::mxmap_municipality_g(df)  
+    if(method == "static"){
+      message("Using mxmap_municipality_s")
+      m <- mexicomapR::mxmap_municipality_s(df, vcol = vcol)  
     }else{
       if(method == "leaflet"){
-        m <- mexicomapR::mxmap_municipality_l(df)
+        message("Using mxmap_municipality_l")
+        m <- mexicomapR::mxmap_municipality_l(df, vcol = vcol)
       }else{
-        if(method == "google"){
-          m <- mexicomapR::mxmap_municipality_g(df)
-        }else{stop("Metod not recognized. Use gglot, leaflet or google")}
-        
+        stop("Metod not recognized. Use static, leaflet or google")}
       }
-    }
-  }else{stop("Geography not recognized. Use state or municipality")}
+    }else{stop("Geography not recognized. Use state or municipality")}
   }
   
   return(m)
 }
 
 
-#' Creates state map
+#' Creates static state map
 #'
-#' Generates a static state map 
+#' Generates a static (ggplot2 object) state map. 
 #' @param df Data frame or vector with information to paint geography. The data must contain an "id" and a "value" column to be matched.
+#' @param vcol Value column with standard evaluation. Defaults to "value".
 #' @author Eduardo Flores
 #' @return ggplot object
 #' @seealso mxmap
 #' @examples
-#' \dontrun{
-#' population_choroplethr <- mxmap("state", data(mxpop2010))
-#' }
+#' # A state map with random data 
+#' utils::data(states_catalog, package = "mexicomapR")
+#' df <- data.frame("id" = states_catalog$id, "value" = runif(n = 32, min = 1, max = 100))
+#' mxmap_state_s(df)
 #' @export
 #'
-mxmap_state_p <- function(df){
+mxmap_state_s <- function(df, vcol = "value"){
+  # Type of map
+  nobject <- 32
+    
+  # checks the data.frame for common mistakes. 
+  # This is a function in backend_functions.R
+  mexicomapR::mx_checkdf(df, vcol = vcol, nobject = nobject)
   
-  if(class(df) == "data.frame" | 
-     class(df) == "matrix"){
-      # clean and fix data frame to merge
-      d <- as.data.frame(df)
-      names(d) <- base::tolower(names(d))
-      if(!("id" %in% names(d))){stop("id column not found in data")}
-      if(!("value" %in% names(d))){stop("value column not found in data")}
-      
+  # change name of value column, to be used by ggplot
+  names(df)[names(df) == vcol] <- "value"
+  
       # get geography to merge 
-      geo <- data("mxstates", package = "mexicomapR")
+      utils::data("states", 
+                  package = "mexicomapR", 
+                  envir = e <- new.env())
+      geo <- sp::spTransform(x = e$states, 
+                         CRSobj = CRS("+proj=longlat +datum=WGS84"))
+      geo <- ggplot2::fortify(geo)
       
-      d <- base::merge(d, mxstates)
-      m <- ggplot(d, 
-                  aes(long, 
-                      lat, group = group)) +
-          geom_polygon(aes(fill = value))  
-        
-  }else{
-   stop("df must be data.frame or matrix")
-  }
+      d <- base::merge(df, geo, by ="id")
+      m <- ggplot2::ggplot(d, 
+                           ggplot2::aes(long, 
+                                        lat, 
+                                        group = group)) +
+          geom_polygon(ggplot2::aes(fill = value))
+      
   return(m)
 }
 
-#' Creates ggplot municipality map
+#' Creates static municipality map
 #'
-#' Generates a static state map 
+#' Generates a static (ggplot2 object) municipality map. 
 #' @param df Data frame or vector with information to paint geography. The data must contain an "id" and a "value" column to be matched.
+#' @param vcol Value column with standard evaluation. Defaults to "value".
 #' @author Eduardo Flores
 #' @return ggplot object
 #' @seealso mxmap
 #' @examples
-#' \dontrun{
-#' population_choroplethr <- mxmap("municipality", data(mxpop2010))
-#' }
+#' # A municipality map with random data 
+#' utils::data(municipality_catalog, package = "mexicomapR")
+#' df <- data.frame("id" = municipality_catalog$id, "value" = runif(n = 2457, min = 1, max = 100))
+#' mxmap_municipality_s(df)
 #' @export
 #'
-mxmap_municipality_p <- function(df){
+mxmap_municipality_s <- function(df, vcol = "value"){
+  # Type of map
+  nobject <- 2457
   
-  if(class(df) == "data.frame" | 
-     class(df) == "matrix"){
-    # clean and fix data frame to merge
-    d <- as.data.frame(df)
-    names(d) <- base::tolower(names(d))
-    if(!("id" %in% names(d))){stop("id column not found in data")}
-    if(!("value" %in% names(d))){stop("value column not found in data")}
-    
-    # get geography to merge 
-    geo <- data("mxmunicipality", package = "mexicomapR")
-    
-    d <- base::merge(d, geo)
-    m <- ggplot(d, 
-                aes(long, 
-                    lat, group = group)) +
-      geom_polygon(aes(fill = value))  
-    
-  }else{
-    stop("df must be data.frame or matrix")
-  }
+  # checks the data.frame for common mistakes. 
+  # This is a function in backend_functions.R
+  mexicomapR::mx_checkdf(df, vcol = vcol, nobject = nobject)
+  
+  # change name of value column, to be used by ggplot
+  names(df)[names(df) == vcol] <- "value"
+  
+  # get geography to merge 
+  utils::data("municipality", 
+              package = "mexicomapR", 
+              envir = e <- new.env())
+  geo <- sp::spTransform(x = e$municipality, 
+                         CRSobj = CRS("+proj=longlat +datum=WGS84"))
+  geo <- ggplot2::fortify(geo)
+  
+  d <- base::merge(df, geo, by ="id")
+  m <- ggplot2::ggplot(d, 
+                       aes(long, 
+                           lat, group = group)) +
+    geom_polygon(aes(fill = value))
+  
   return(m)
 }
 
@@ -142,15 +151,17 @@ mxmap_municipality_p <- function(df){
 #' @export
 #'
 mxmap_state_l <- function(df){
+  # Type of map
+  nobject <- 32
   
-  if(class(df) == "data.frame" | 
-       class(df) == "matrix"){
-    # clean and fix data frame to merge
-    d <- as.data.frame(df)
-    names(d) <- base::tolower(names(d))
-    if(!("id" %in% names(d))){stop("id column not found in data")}
-    if(!("value" %in% names(d))){stop("value column not found in data")}
-    
+  # checks the data.frame for common mistakes. 
+  # This is a function in backend_functions.R
+  mexicomapR::mx_checkdf(df, vcol = vcol, nobject = nobject)
+  
+  
+  
+  
+  
     # get geography to merge 
     # change 
     # geo <- data("mxmunicipality", package = "mexicomapR")
@@ -172,33 +183,11 @@ mxmap_state_l <- function(df){
     mx %>% 
       addPolygons(lng = m$long, lat = m$lat) %>%
       addPolygons(lng = m2$long, lat = m2$lat)
-    
   
-    
-  }else{
-    stop("df must be data.frame or matrix")
-  }
   return(mx)
 }
 
 
-
-
-
-
-#' Creates state catalog
-#'
-#' Uses \code{data(mxgeography)} to generate a data.frame with two columns: id as factor and name of state, on which to join data. This assures the user will use the correct id's to pass to \code{mxmap()}.
-#' @author Eduardo Flores
-#' @return data.frame
-#' @seealso mxgeography
-#' @examples
-#' mxstate_catalog()
-#' @export
-#'
-mxstate_catalog <- function(){
-  #mxgeography
-}
 #' Creates municipality catalog
 #'
 #' Uses \code{data(mxgeography)} to generate a data.frame with two columns: id as factor and name of municipality, on which to join data. This assures the user will use the correct id's to pass to \code{mxmap()}.
